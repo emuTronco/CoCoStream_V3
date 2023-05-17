9a80fdff13e827a753f88d5857c00ff5d859e014
David Patron Fernandez <tronez98@gmail.com>
2023-05-17 13:36:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cocostream_v3/Home.java
@@ -0,0 +1,32 @@
+package com.example.cocostream_v3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    EditText etYoutube;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+//        etYoutube = findViewById(R.id.editTextLinkYoutube);
+//        etYoutube.setText("https://www.youtube.com/watch?v=s37IMt_Ec-o");
+    }
+
+    public void onClickAceptar(View v) {
+//        EditText etYoutube = findViewById(R.id.editTextLinkYoutube);
+//        EditText etTwitch = findViewById(R.id.editTextCanalTwitch);
+//        String videoYoutube = etYoutube.getText().toString();
+//        String canalTwitch = etTwitch.getText().toString();
+//
+//        Intent i = new Intent(this, StreamPlayer.class);
+//        i.putExtra("videoYoutube", videoYoutube);
+//        i.putExtra("canalTwitch", canalTwitch);
+//        startActivity(i);
+    }
+}
